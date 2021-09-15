07fcf3529ab9c8646066abaaf8ae17f9341cdfcb
Alexander Jordan <JordanLAlexander@gmail.com>
2021-09-15 18:28:45 -0500

    Sneaky snek

---
+++ js/gameengine.js
@@ -0,0 +1,256 @@
+class GameObject {
+
+    constructor(id){
+        this.x = 0;
+        this.y = 0;
+        this.id = id;
+        this.img = undefined;
+        this.vector = [0,0,0]; // x, y, magnitude
+        this.drawdepth = 0;
+        this.lastphysicsaction = 0;
+        this.hascollider = false;
+        this.colliderwidth = 0;
+        this.colliderheight = 0;
+        this.colliderx = 0;
+        this.collidery = 0;
+        this.rotation = 0;
+        this.render = true;
+        this.persist = false;
+    }
+
+    // Take a string, make it an img.  Ez.
+    addImage(img){
+        let hold = new Image();
+        hold.src = img;
+        this.img = hold;
+    }
+
+}
+
+class GameEngine {
+
+    constructor(gameloop, canvas){
+        this.configuration = {
+            tickRate: 1,
+            drawRate: 60,
+            debug: false
+        };
+        this.gameLoop = gameloop;
+        this.gameObjs = {};
+
+        this.lasttick = Date.now();
+        this.deltatime = 0;
+        this.lastdraw = Date.now();
+
+        this.canvas = canvas;
+
+        this.inputs = [];
+        this.keybinds = {};
+        this.clickedObject = false;
+        document.addEventListener("keydown", this.handleKeyPress.bind(this));
+        this.canvas.addEventListener("click", this.clickHandler.bind(this));
+    }
+
+    // Quick and dirty wrapper for console.log
+    debuglog(msg){
+        if(this.configuration.debug){
+            console.log(msg);
+        }
+    }
+
+    // Run the engine.  Should leave this manual so we can config before start
+    start(tick = this.configuration.tickRate){
+        console.log("Engine spin-up");
+        tick = 1000/tick;
+        this.lasttick = Date.now();
+        // Must use bind or we'll have the wrong context
+        this.interval = setInterval(this.gametick.bind(this), tick);
+    }
+
+    // Mostly useful for debugging.
+    stop(){
+        clearInterval(this.interval);
+    }
+
+    // This is our main game loop, for the engine side of things.
+    gametick(){
+        this.deltatime = Date.now() - this.lasttick;
+        this.gameLoop();
+
+        //draw the game.
+        if(Date.now() - this.lastdraw >= 1000/this.configuration.drawRate){
+            this.drawGame();
+        }
+
+        // We only want to store and track up to 10 inputs.
+        while(this.inputs.length > 10){
+            this.inputs.pop(0);
+        }
+
+        // Completion time of tick
+        this.lasttick = Date.now();
+    }
+
+    drawGame(){
+
+        // Sort our game objects by render depth and filter unrenderables
+        let sortable = [];
+        for (let x in this.gameObjs){
+            // We don't want to mess with unrenderables
+            if(this.gameObjs[x].render && this.gameObjs[x].img !== undefined){
+                
+                // We need to make sure we actually have an image in there.
+                // We cant be sure that addImage() was used.
+                if(typeof this.gameObjs[x].img == "string"){
+                    let hold = new Image();
+                    hold.src = this.gameObjs[x].img;
+                    this.gameObjs[x].img = hold;
+                }
+                sortable.push(this.gameObjs[x]);
+
+            }
+        }
+
+        sortable.sort((a,b) => (a.drawdepth > b.drawdepth) ? 1 : -1);
+
+        let ctx = this.canvas.getContext("2d");
+        //Just to be really really sure
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+        // Clear the canvas, otherwise we're just rendering on top of last frame.
+        ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
+        
+        for(let i = 0; i < sortable.length; i++){
+
+            // Save context because we're about to mess about
+            ctx.save();
+
+            //deal with rotations
+            // Move context to center of image to be rotated.
+            ctx.translate(sortable[i].x + sortable[i].img.width/2,sortable[i].y + sortable[i].img.height/2);
+            // Rotate
+            ctx.rotate(sortable[i].rotation*Math.PI/180);
+            // Move context back to the corner of the image to avoid more problems with rendering.
+            ctx.translate(-sortable[i].img.width/2, -sortable[i].img.height/2);
+
+            //draw image
+            ctx.drawImage(sortable[i].img, 0, 0);
+
+            // Throw away translations/rotations.
+            ctx.restore();
+
+        }
+        
+        this.lastdraw = Date.now();
+    }
+
+    get gameObjectIDs(){
+        return Object.keys(this.gameObjs);
+    }
+
+    // Create new game objects.  Dirty, but works.
+    newGameObject(id = "newObject"){
+        let digit = 2;
+        let flag = false;
+        if(this.gameObjs[id] !== undefined){
+            flag = true;
+            while(this.gameObjs[id+digit] !== undefined ){
+                digit++;
+            }
+        }
+
+        if(flag){
+            id = id+digit;
+        }
+
+        this.gameObjs[id] = new GameObject(id);
+        return this.gameObjs[id];
+    }
+
+    clearGameObjects(){
+        for (let x in this.gameObjs){
+            if (this.gameObjs[x].persist == false){
+                delete this.gameObjs[x];
+            }
+        }
+    }
+
+    handleKeyPress(kp){
+        let keycode = String.fromCharCode(kp.keyCode);
+
+        switch (kp.keyCode){
+            case 37:
+                keycode = "ARROW-LEFT";
+                break;
+
+            case 38:
+                keycode = "ARROW-UP";
+                break;
+            
+            case 39:
+                keycode = "ARROW-RIGHT";
+                break;
+
+            case 40:
+                keycode = "ARROW-DOWN";
+                break;
+        }
+
+
+        if(keycode in this.keybinds){
+            this.inputs.push(keycode);
+        }
+
+    }
+
+    addKeybind(key){
+        this.keybinds[key] = true;
+    }
+
+    getInputs(){
+        let hold = this.inputs;
+        this.inputs = [];
+        return hold;
+    }
+
+    getLastInput(){
+        if(this.inputs.length > 0){
+            return this.inputs.pop();
+        }
+        return false;
+    }
+    
+    clickHandler(event){
+        
+        for(let x in this.gameObjs){
+            if(this.gameObjs[x].hascollider){
+
+                // Correct for the weirdness of click coordinates
+                let rect = this.canvas.getBoundingClientRect();
+                let clickx = event.x - rect.left;
+                let clicky =  event.y - rect.top;
+
+                if((this.gameObjs[x].x < clickx && clickx < this.gameObjs[x].x + this.gameObjs[x].colliderwidth) &&
+                   (this.gameObjs[x].y < clicky && clicky < this.gameObjs[x].y + this.gameObjs[x].colliderheight)){
+                       if(this.clickedObject){
+                           if(this.gameObjs[x].drawdepth > this.gameObjs[this.clickedObject].drawdepth){
+                               this.clickedObject = this.gameObjs[x].id;
+                           }
+                       }else{
+                            this.clickedObject = this.gameObjs[x].id;
+                        }
+                       this.inputs.push("ClickCollusion");
+                   }
+            }
+        }
+    }
+
+    getClickedObject(){
+        if (this.clickedObject){
+            let hold = this.gameObjs[this.clickedObject];
+            this.clickedObject = false;
+            return hold;
+        }
+        return false;
+    }
+}
---
+++ js/snake.js
@@ -0,0 +1,319 @@
+let snake = new GameEngine(game, document.getElementById("snakeCanvas"));
+(function(){
+    let snakeCanvas = document.getElementById("snakeCanvas");
+    let ctx = snakeCanvas.getContext("2d");
+    ctx.moveTo(0, 0);
+    snake.configuration.debug = true;
+    snake.configuration.tickRate = 100;
+    snake.canvas = document.getElementById("snakeCanvas");
+    createUI();
+
+    snake.addKeybind("W");
+    snake.addKeybind("A");
+    snake.addKeybind("S");
+    snake.addKeybind("D");
+    snake.addKeybind("ARROW-LEFT");
+    snake.addKeybind("ARROW-RIGHT");
+    snake.addKeybind("ARROW-UP");
+    snake.addKeybind("ARROW-DOWN");
+
+    snake.newGameObject("GameState");
+    snake.gameObjs["GameState"].persist = true;
+    snake.gameObjs["GameState"].state = 1; //lobby
+
+    snake.start(gameloop = game());
+})();
+
+function game(){
+    let state = snake.gameObjs["GameState"]
+    if(state.state == 1){
+        let input = snake.getLastInput();
+        if(input == "ClickCollusion"){
+            if(snake.getClickedObject().id == "NewGame"){
+                prepGame();
+            }
+        }
+    }
+    if(state.state == 2){
+        if(Date.now() - state.lastMoveTick > 1000/state.snakeSpeed){
+            let input = snake.getLastInput();
+            state.lastMoveTick = Date.now();
+
+            let xmov = 0;
+            let ymov = 0;
+            let newvec = snake.gameObjs["SnakeHead"].vector;
+            if(input){
+                switch(input){
+                    case "W":
+                        ymov = -50;
+                        newvec = [0,-1,0]
+                        break;
+                    case "A":
+                        xmov = -50;
+                        newvec = [-1,0,0]
+                        break;
+                    case "S":
+                        ymov = 50;
+                        newvec = [0,1,0]
+                        break;
+                    case "D":
+                        xmov = 50;
+                        newvec = [1,0,0]
+                        break;
+                }
+            }
+
+            for(let obj in snake.gameObjs){
+                if(obj == "SnakeHead"){
+                   
+                        snake.gameObjs["SnakeHead"].vector = newvec;
+                        if(newvec[1] == -1){
+                            snake.gameObjs["SnakeHead"].rotation = 0;
+                            snake.gameObjs["SnakeHead"].y += -50;
+                            if(!boundsCheck(snake.gameObjs["SnakeHead"].x, snake.gameObjs["SnakeHead"].y) || !snakeCollusionCheck()){
+                                //Game over man.
+                                snake.gameObjs["SnakeHead"].y += 50;
+                                state.state = 3;
+                                return;
+                            }
+                        }
+                        if(newvec[1] == 1){
+                            snake.gameObjs["SnakeHead"].rotation = 180;
+                            snake.gameObjs["SnakeHead"].y += 50;
+                            if(!boundsCheck(snake.gameObjs["SnakeHead"].x, snake.gameObjs["SnakeHead"].y) || !snakeCollusionCheck()){
+                                //Game over man.
+                                snake.gameObjs["SnakeHead"].y += -50;
+                                state.state = 3;
+                                return;
+                            }
+                        }
+                        if(newvec[0] == -1){
+                            snake.gameObjs["SnakeHead"].rotation = 270;
+                            snake.gameObjs["SnakeHead"].x += -50;
+                            if(!boundsCheck(snake.gameObjs["SnakeHead"].x, snake.gameObjs["SnakeHead"].y) || !snakeCollusionCheck()){
+                                //Game over man.
+                                snake.gameObjs["SnakeHead"].x += 50;
+                                state.state = 3;
+                                return;
+                            }
+                        }
+                        if(newvec[0] == 1){
+                            snake.gameObjs["SnakeHead"].rotation = 90;
+                            snake.gameObjs["SnakeHead"].x += 50;
+                            if(!boundsCheck(snake.gameObjs["SnakeHead"].x, snake.gameObjs["SnakeHead"].y) || !snakeCollusionCheck()){
+                                //Game over man.
+                                snake.gameObjs["SnakeHead"].x += -50;
+                                state.state = 3;
+                                return;
+                            }
+                        }
+                        
+                }
+
+                if(obj.includes("SnakeSection")){
+                    let current = snake.gameObjs[obj];
+                    if(current.vector[0] == 1){
+                        current.x += 50;
+                        current.rotation = 0;
+                    }
+                    if(current.vector[0] == -1){
+                        current.x += -50;
+                        current.rotation = 180;
+                    }
+                    if(current.vector[1] == 1){
+                        current.y += 50;
+                        current.rotation = 90;
+                    }
+                    if(current.vector[1] == -1){
+                        current.y += -50;
+                        current.rotation = 90;
+                    }
+
+                    if(current.nextSegment === undefined){
+                        while(current.id != "SnakeHead"){
+                            current.vector = current.previousSection.vector;
+                            current = current.previousSection;
+                        }
+                    }
+                }
+            }
+
+            if(foodCheck()){
+
+            }
+
+            snake.inputs = [];
+        }  
+    }
+
+    if(state.state == 3){
+        gameOverMan();
+    }
+}
+
+function snakeCollusionCheck(){
+    let xy = [snake.gameObjs["SnakeHead"].x, snake.gameObjs["SnakeHead"].y];
+
+    for(let obj in snake.gameObjs){
+        if(obj.includes("SnakeSection")){
+            let current = snake.gameObjs[obj];
+            if(xy[0] == current.x && xy[1] == current.y){
+                return false;
+            }
+        }
+    }
+    return true;
+    
+}
+
+function foodCheck(){
+    let head = snake.gameObjs["SnakeHead"];
+    let food = snake.gameObjs["Food"];
+
+    if(head.x == food.x && head.y == food.y){
+        snake.gameObjs["GameState"].score += 1;
+        let snek = snake.newGameObject("SnakeSection");
+        snek.addImage("/portfolio/assets/img/snek.png");
+        snek.nextSegment = undefined;
+        snek.hascollider = true;
+        snek.colliderwidth = 50;
+        snek.colliderheight = 50;
+        
+        let last = snake.gameObjs["SnakeSection2"];
+
+        while(last.nextSegment !== undefined){
+            last = last.nextSegment;
+        }
+
+        snek.previousSection = last;
+        snek.previousSection.nextSegment = snek;
+        snek.x = last.x;
+        snek.y = last.y;
+
+        let xcell = Math.floor(Math.random()*12);
+        let ycell = Math.floor(Math.random()*8);
+        let freepos = Array.from(Array(12), () => new Array(8));
+
+        for(let i = 0; i < 12; i++){
+            for(let j = 0; j < 8; j++){
+                freepos[i][j] = true; 
+            }
+        }
+        
+        for(obj in snake.gameObjs){
+            freepos[snake.gameObjs[obj].x/50][snake.gameObjs[obj].y/50] = false;
+        }
+        
+        while(freepos[xcell][ycell] == false){
+            xcell = Math.floor(Math.random()*12);
+            ycell = Math.floor(Math.random()*8);
+        }
+
+        food.x = xcell*50;
+        food.y = ycell*50;
+    }
+}
+
+function boundsCheck(x,y){
+    if(x < 0 || y < 0){
+        return false;
+    }
+
+    if(x > 550 || y > 350){
+        return false;
+    }
+    return true;
+}
+
+function gameOverMan(){
+    snake.clearGameObjects();
+    snake.gameObjs["GameState"].state = 1;
+    createUI();
+
+}
+
+function prepGame(){
+    snake.clearGameObjects();
+    snake.gameObjs["GameState"].state = 2;
+    snake.gameObjs["GameState"].lastMoveTick = Date.now();
+    snake.gameObjs["GameState"].snakeSpeed = 2;
+    snake.gameObjs["GameState"].score = 0;
+
+    let snek = snake.newGameObject("SnakeHead");
+    snek.addImage("/portfolio/assets/img/snekhead.png");
+    snek.hascollider = true;
+    snek.colliderwidth = 50;
+    snek.colliderheight = 50;
+    snek.x = 300;
+    snek.y = 200;
+    snek.vector = [1,0,0];
+    snek.rotation = 90;
+    snek.nextSegment = undefined;
+
+    for(let i = 1; i < 3; i++){
+        snek = snake.newGameObject("SnakeSection");
+        snek.addImage("/portfolio/assets/img/snek.png");
+        snek.previousSection = snake.gameObjs[Object.keys(snake.gameObjs)[Object.keys(snake.gameObjs).length-2]];
+        snek.previousSection.nextSegment = snek;
+        snek.nextSegment = undefined;
+        snek.hascollider = true;
+        snek.colliderwidth = 50;
+        snek.colliderheight = 50;
+        snek.vector = [1,0,0];
+        snek.x = 300-(50*i);
+        snek.y = 200;
+        snek.rotation = 0;
+    }
+
+    snake.gameObjs["SnakeHead"].nextSegment = snake.gameObjs["SnakeSection"];
+
+    let xcell = Math.floor(Math.random()*12) * 50;
+    let ycell = Math.floor(Math.random()*8);
+
+    while(ycell == 5){
+        ycell = Math.floor(Math.random()*8);
+    }
+
+    ycell = ycell*50;
+
+    let food = snake.newGameObject("Food");
+    food.hascollider = true;
+    food.x = xcell;
+    food.y = ycell;
+    food.addImage("/portfolio/assets/img/food.png");
+
+}
+
+function createUI(){
+    let snakeCanvas = document.getElementById("snakeCanvas");
+    let width = snakeCanvas.width;
+    let height = snakeCanvas.height;
+
+    let newgame = snake.newGameObject("NewGame");
+    newgame.addImage("/portfolio/assets/img/newgame.png");
+
+    newgame.hascollider = true;
+    newgame.x = snakeCanvas.width/2 - 75;
+    newgame.y = snakeCanvas.height/2 - 50;
+    newgame.colliderwidth = 150;
+    newgame.colliderheight = 50;
+
+
+    let snek = snake.newGameObject("SnakeHead");
+    snek.addImage("/portfolio/assets/img/snekhead.png");
+    snek.hascollider = true;
+    snek.colliderwidth = 50;
+    snek.colliderheight = 50;
+    snek.x = 0;
+    snek.y = 0;
+    snek.rotation = 270;
+
+    for(let i = 0; i < 13; i++){
+        snek = snake.newGameObject("SnakeSection");
+        snek.addImage("/portfolio/assets/img/snek.png");
+        snek.x = 50+(50*i);
+        snek.y = 0;
+    }
+
+
+}
