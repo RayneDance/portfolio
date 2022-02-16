b394cc1bc85d99ff28d0036f9a4413728eec269e
Alexander Jordan <JordanLAlexander@gmail.com>
2022-02-16 12:06:04 -0600

    Added reporgrammable dots.
    Can choose dot source locations.

--- js/triangles.js
+++ js/triangles.js
@@ -3,13 +3,16 @@ class triangleGenerator {
         this.canvas = canvas;
     }
 
-    draw(dots){
+    draw(dots, dotPairs){
 
         let ctx = this.canvas.getContext("2d");
         //Just to be really really sure
         ctx.setTransform(1, 0, 0, 1, 0, 0);
 
-        const randomMoves = [[1, this.canvas.height-1], [this.canvas.width-1, this.canvas.height-1], [this.canvas.width/2, 1]]
+        //600x400
+        //const randomMoves = [[0, this.canvas.height], [this.canvas.width, this.canvas.height], [this.canvas.width/2, 0]]
+        //const randomMoves = [[100,100], [300,1], [500,100], [175,350],[425,350]]
+        const randomMoves = dotPairs;
 
         let dot = [Math.floor(Math.random()*this.canvas.width), Math.floor(Math.random()*this.canvas.height)];
         let color = document.getElementById("colorPicker").value;
@@ -19,15 +22,18 @@ class triangleGenerator {
                 ctx.fillStyle = color;
                 ctx.fillRect(dot[0], dot[1], 1, 1);
             }
-            let newDirection = randomMoves[Math.floor(Math.random()*3)];
+            let newDirection = randomMoves[Math.floor(Math.random()*dotPairs.length)];
             
-            dot = [(dot[0]+newDirection[0])/2, (dot[1]+newDirection[1])/2];
+            dot = [dot[0]+Math.floor((newDirection[0] - dot[0])/2), dot[1]+Math.floor((newDirection[1] - dot[1])/2)];
+            //console.log(dot);
         }
     }
     clear(){
         let ctx = this.canvas.getContext("2d");
         
         ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
 }
 
@@ -37,8 +43,15 @@ window.addEventListener("load", function(){
 
     generateButton.addEventListener("click", () => {
         let triangles = new triangleGenerator(document.getElementById("triangleCanvas"));
+        let dots = document.getElementsByClassName("point-input");
+        let dotPairs = [];
 
-        triangles.draw(document.getElementById("dotsRange").value);
+        for (let i = 0; i < dots.length; i+=2){
+            dotPairs.push([dots[i].value, dots[i+1].value]);
+        }
+
+        console.log(dotPairs);
+        triangles.draw(document.getElementById("dotsRange").value, dotPairs);
     });
 
     let clearCanvas = document.getElementById("clearCanvas");
